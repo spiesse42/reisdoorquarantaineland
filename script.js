c3b57ca340fc6b084205b18933fb727843bbf98b
spiesse42 <davidspiesschaert1@gmail.com>
2026-02-17 22:15:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,7 @@ const huidigeStatus = {
 
 // DEEL 2: Updates
 const updates = [
-    {
-        datum: "16 Februari 2026",
-        tijd: "08:45u",
-        bericht: `
-Goedemorgen allemaal. 
-Ik ben net aangekomen in het ziekenhuis, mooi op tijd om me tegen 9u aan te melden op de afdeling Hematologie. Omdat we alletwee weten dat de ziekenhuiskoffie net 
-iets minder lekker is dan deze thuis beslissen we om onze neus te volgen en nog rap een koffieke te gaan drinken in de cafetaria. Slurp slurp en hop naar verdieping 2. 
-
-Gepakt & gezakt gelijk 2 muilezels konden we dan vertrekken richting verdieping 2. De verpleegster stond ons al op te wachten aan de schuifdeur, klaar om ons naar
-mijn buitenverblijf te begeleiden. Zon, zee en strand wordt het niet helaas. 24 graden en droog, dat wel voor de komende weken.
-
-Op naar de installatie van de kamer en hetgeen daarop volgt.
-        `,
-        fotoUrl: "afbeeldingen/Intake.jpeg"
-    },
-    // HIERONDER KAN JE EEN NIEUWE UPDATE TOEVOEGEN:
-    /*
-    {
+   {
         datum: "16 Februari 2026",
         tijd: "10:45u",
         bericht: `
@@ -44,7 +27,24 @@ Zo ben je vlug patiënt, nou nou. Deze namiddg nog een zenuw- en spieronderzoek 
 Op naar het volgende zou ik zeggen...
         `,
         fotoUrl: "afbeeldingen/Intake.jpeg"
+    }, {
+        datum: "16 Februari 2026",
+        tijd: "08:45u",
+        bericht: `
+Goedemorgen allemaal. 
+Ik ben net aangekomen in het ziekenhuis, mooi op tijd om me tegen 9u aan te melden op de afdeling Hematologie. Omdat we alletwee weten dat de ziekenhuiskoffie net 
+iets minder lekker is dan deze thuis beslissen we om onze neus te volgen en nog rap een koffieke te gaan drinken in de cafetaria. Slurp slurp en hop naar verdieping 2. 
+
+Gepakt & gezakt gelijk 2 muilezels konden we dan vertrekken richting verdieping 2. De verpleegster stond ons al op te wachten aan de schuifdeur, klaar om ons naar
+mijn buitenverblijf te begeleiden. Zon, zee en strand wordt het niet helaas. 24 graden en droog, dat wel voor de komende weken.
+
+Op naar de installatie van de kamer en hetgeen daarop volgt.
+        `,
+        fotoUrl: "afbeeldingen/Intake.jpeg"
     },
+    // HIERONDER KAN JE EEN NIEUWE UPDATE TOEVOEGEN:
+    /*
+    
     */
 ];
 
@@ -121,4 +121,5 @@ function laadUpdates() {
 
     timelineDiv.innerHTML = html;
 }
+
 
