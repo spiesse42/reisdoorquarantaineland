f0cdeed939129e5ed6bef4e2636a5749ff18d219
spiesse42 <davidspiesschaert1@gmail.com>
2026-02-17 09:12:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const updates = [
         // VORBEELD VAN EEN FOTO TOEVOEGEN:
         // Zorg dat de bestandsnaam exact klopt met wat je uploadt.
         // Als je geen foto hebt, verwijder je deze regel 'fotoUrl' gewoon.
-        fotoUrl: "https://github.com/spiesse42/reisdoorquarantaineland/blob/main/afbeeldingen/Intake.jpeg" 
+        fotoUrl: https://github.com/spiesse42/reisdoorquarantaineland/blob/main/afbeeldingen/Intake.jpeg
     },
     {
         datum: "18 Februari 2026",
@@ -93,4 +93,5 @@ function laadUpdates() {
 
     timelineDiv.innerHTML = html;
 }
+
 
