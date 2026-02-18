512cbfdb9115860b19e5b473109688aa9f43e7d8
spiesse42 <davidspiesschaert1@gmail.com>
2026-02-18 11:11:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-u/* CONFIGURATIE VOOR SPIESSE */
+/* CONFIGURATIE VOOR SPIESSE */
 
 // DEEL 1: Status
 const huidigeStatus = {
@@ -9,7 +9,7 @@ const huidigeStatus = {
 
 // DEEL 2: Updates
 const updates = [
-   {
+    {
         datum: "18 Februari 2026",
         tijd: "10:36u",
         bericht: `
@@ -17,7 +17,7 @@ Nacht 2 van ... zit er op. En het was terug nen goeien.
 
 Gisterennamiddag en -avond niet al te veel last ondervonden van de chemo. Alles is goed verlopen, op een kwartier was het zakske al leeg en mijn maag vol:
 3 potjes ijscrème en nen halve liter koude limonade met ijsblokken. Allemaal broodnodig om de mondslijmvliezen zoveel mogelijk te vrijwaren van de chemo,
-het is namelijk een vaak voorkomend probleem dat de slijmvliezen van het spijsverteringsstelsel kunnen beschadigd rakenn en dan wordt het wel moeilijk
+het is namelijk een vaak voorkomend probleem dat de slijmvliezen van het spijsverteringsstelsel kunnen beschadigd raken en dan wordt het wel moeilijk
 om te eten.
 
 Na het deugdoend bezoek van mijn piebe kon ik rustig aan de avond beginnen. Rond 21u iets gekregen tegen de misselijkheid, had toch wat last van 'een zware maag'
@@ -29,7 +29,7 @@ Spiesse wordt een stukske herboren, wel wel wel...
 
 Laat maar komen, ben er klaar voor!
         `,
-        // NIEUW: Meerdere foto's zet je tussen vierkante haken [ ], gescheiden door een komma.
+        // NIEUW: Meerdere foto's voor deze update
         fotos: [
             "afbeeldingen/cocktail chemo.JPG", 
             "afbeeldingen/calorietjes.JPG"
@@ -46,10 +46,10 @@ Binnen een paar uur begint de aftelklok echt wel dicht bij 11u te komen. Belangr
 mijne binnenkant voor te bereiden (lees: kapot te maken) zodat de stamcellen vanaf donderdag hun werk kunnen beginnen doen.
 
 Laat maar komen, ben er klaar voor!
-        `,
-
+        `
+        // Geen foto's bij deze update
     }, 
-{
+    {
         datum: "16 Februari 2026",
         tijd: "10:45u",
         bericht: `
@@ -62,16 +62,16 @@ Verpleegster 1 trok bloed terwijl verpleegster 2 de parameters nam.
 Na een klein half uur mocht ik dan naar 'beneden', naar het operatiekwartier. Daar werd de centrale catheter gestoken net onder mijn sleutelbeen
 om daar dan de chemo op aan te sluiten morgen & de stamcellen ook via deze weg ook terug te kunnen geven.
 
-Zo ben je vlug patiënt, nou nou. Deze namiddg nog een zenuw- en spieronderzoek om al dan niet zenuwschade door behandeling te laten vaststellen.
+Zo ben je vlug patiënt, nou nou. Deze namiddag nog een zenuw- en spieronderzoek om al dan niet zenuwschade door behandeling te laten vaststellen.
 
 Op naar het volgende zou ik zeggen...
         `,
         fotoUrl: "afbeeldingen/centrale catheter.JPG"
     }, 
-{
-    datum: "16 Februari 2026",
-    tijd: "08:45u",
-    bericht: `
+    {
+        datum: "16 Februari 2026",
+        tijd: "08:45u",
+        bericht: `
 Goedemorgen allemaal. 
 Ik ben net aangekomen in het ziekenhuis, mooi op tijd om me tegen 9u aan te melden op de afdeling Hematologie. Omdat we alletwee weten dat de ziekenhuiskoffie net 
 iets minder lekker is dan deze thuis beslissen we om onze neus te volgen en nog rap een koffieke te gaan drinken in de cafetaria. Slurp slurp en hop naar verdieping 2. 
@@ -82,16 +82,12 @@ mijn buitenverblijf te begeleiden. Zon, zee en strand wordt het niet helaas. 24 
 Op naar de installatie van de kamer en hetgeen daarop volgt.
         `,
         fotoUrl: "afbeeldingen/Intake.jpeg"
-    },
-    // HIERONDER KAN JE EEN NIEUWE UPDATE TOEVOEGEN:
-    /*
-    
-    */
+    }
 ];
 
 /* ----------------------------------------------------------
    TECHNISCHE LOGICA 
-   (Nu met ondersteuning voor meerdere foto's!)
+   (Alles hieronder regelt de weergave op de website)
    ----------------------------------------------------------
 */
 
